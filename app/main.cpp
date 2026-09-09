220540bbe8bb9bf8f96523cbb66041f57f76f7b2
RammusOTP <emmanuelphillips33@gmail.com>
2026-09-09 10:39:31 +0100

    restructured code into function in a hpp and cpp file

--- app/main.cpp
+++ app/main.cpp
@@ -1,7 +1,5 @@
-#include <algorithm>
-#include <cmath>
+#include "monte_carlo.hpp"
 #include <iostream>
-#include <random>
 
 int main() {
   double spot{100.0};
@@ -11,31 +9,9 @@ int main() {
   double expiry{1.0};
   int simulations{10000};
 
-  std::random_device rd;
-  std::mt19937 gen{rd()};
-  std::normal_distribution<double> dist(0.0, 1.0);
-
-  double total_payoff{0.0};
-
-  double drift{(rate - 0.5 * volatility * volatility) * expiry};
-  double sqrt_expiry{std::sqrt(expiry)};
-
-  for (int i{0}; i < simulations; ++i) {
-    double standard_normal{dist(gen)};
-
-    double diffusion{volatility * sqrt_expiry * standard_normal};
-
-    double terminal_price{spot * std::exp(drift + diffusion)};
-
-    double payoff{std::max(terminal_price - strike, 0.0)};
-
-    total_payoff += payoff;
-  }
-
-  double average_payoff{total_payoff / simulations};
-  double option_price{std::exp(-rate * expiry) * average_payoff};
-
-  std::cout << "Monte Carlo European Call Price: " << option_price << '\n';
+  double option_price{
+      monte_carlo(spot, strike, volatility, rate, expiry, simulations)};
+  std::cout << "Option price: " << option_price << '\n';
 
   return 0;
 }
