15cf85870a6d129f011ebe4bf137f919e96c1e46
RammusOTP <emmanuelphillips33@gmail.com>
2026-09-14 11:42:55 +0100

    implemented black scholes

--- src/black_scholes.cpp
+++ src/black_scholes.cpp
@@ -1,8 +1,20 @@
 #include "black_scholes.hpp"
+#include <cmath>
+
+namespace {
+double N(double x) { return 0.5 * std::erfc(-x / std::sqrt(2.0)); }
+} // namespace
 
 double black_scholes(double spot, double strike, double volatility, double rate,
                      double expiry) {
 
-  double option_price{};
+  double d_1{(std::log(spot / strike) +
+              (rate + (0.5 * volatility * volatility)) * expiry) /
+             (volatility * std::sqrt(expiry))};
+
+  double d_2{d_1 - (volatility * std::sqrt(expiry))};
+
+  double option_price{(spot * N(d_1)) -
+                      (strike * std::exp(-rate * expiry) * N(d_2))};
   return option_price;
 }
