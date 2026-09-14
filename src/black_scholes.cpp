ca3b7619b3e7a2a33b5a19473132898d24299dba
RammusOTP <emmanuelphillips33@gmail.com>
2026-09-14 11:01:22 +0100

    wrote small boilerplate

--- src/black_scholes.cpp
+++ src/black_scholes.cpp
@@ -1,1 +1,8 @@
 #include "black_scholes.hpp"
+
+double black_scholes(double spot, double strike, double volatility, double rate,
+                     double expiry) {
+
+  double option_price{};
+  return option_price;
+}
