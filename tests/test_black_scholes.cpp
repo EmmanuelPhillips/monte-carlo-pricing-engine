a7d9df7093977b6c68dd7482788736165edad967
RammusOTP <emmanuelphillips33@gmail.com>
2026-09-22 12:28:37 +0100

    modified tests to use OptionParameters

--- tests/test_black_scholes.cpp
+++ tests/test_black_scholes.cpp
@@ -4,35 +4,25 @@
 #include <iostream>
 
 void test_known_value() {
-  constexpr double spot{100};
-  constexpr double strike{100};
-  constexpr double volatility{0.20};
-  constexpr double rate{0.05};
-  constexpr double expiry{1.0};
+  OptionParameters parameters{100.0, 100.0, 0.2, 0.05, 1.0};
 
   constexpr double expected_option_price{10.4506};
   constexpr double tolerance = 1e-4;
 
-  double option_price{black_scholes(spot, strike, volatility, rate, expiry)};
+  double option_price{black_scholes(parameters)};
 
   assert(std::abs(option_price - expected_option_price) < tolerance);
   std::cout << "known value test passed.\n";
 }
 
 void test_higher_volatility_increases_price() {
-  constexpr double spot{100};
-  constexpr double strike{100};
-  double volatility{0.20};
-  constexpr double rate{0.05};
-  constexpr double expiry{1.0};
+  OptionParameters parameters{100.0, 100.0, 0.2, 0.05, 1.0};
 
-  double previous_option_price{
-      black_scholes(spot, strike, volatility, rate, expiry)};
+  double previous_option_price{black_scholes(parameters)};
 
   for (int i{0}; i < 5; ++i) {
-    volatility += 0.1;
-    double current_option_price{
-        black_scholes(spot, strike, volatility, rate, expiry)};
+    parameters.set_volatility(parameters.get_volatility() + 0.1);
+    double current_option_price{black_scholes(parameters)};
     assert(current_option_price > previous_option_price);
     previous_option_price = current_option_price;
   }
@@ -40,18 +30,12 @@ void test_higher_volatility_increases_price() {
 }
 
 void test_longer_expiry_increases_price() {
-  constexpr double spot{100};
-  constexpr double strike{100};
-  constexpr double volatility{0.20};
-  constexpr double rate{0.05};
-  double expiry{1.0};
+  OptionParameters parameters{100.0, 100.0, 0.2, 0.05, 1.0};
 
-  double previous_option_price{
-      black_scholes(spot, strike, volatility, rate, expiry)};
+  double previous_option_price{black_scholes(parameters)};
   for (int i{0}; i < 5; ++i) {
-    expiry += 1;
-    double current_option_price{
-        black_scholes(spot, strike, volatility, rate, expiry)};
+    parameters.set_expiry(parameters.get_expiry() + 1.0);
+    double current_option_price{black_scholes(parameters)};
     assert(current_option_price > previous_option_price);
     previous_option_price = current_option_price;
   }
